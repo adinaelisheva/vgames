a71472944c32ad47d68504b5ac9b1b336c95ca88
Adina Karantza <adina.elisheva@gmail.com>
2026-01-05 01:43:55 +0000

    updated styles for 2026

--- index.php
+++ index.php
@@ -11,86 +11,88 @@
   </head>
   <body>
     <div class="header"><h2>The challenge so far</h2></div>
-    <div class="main withCols">
-      <div class="col1">
-        <div class="widgetContainer">
+    <div class="main">
+      <div class="widgetContainer">
           <?php 
             include("widgetCode.php");
           ?>
         </div>
-        <?php 
-          include("setup.php");
-          $year = date("Y");
-          $str = "SELECT * FROM `logs` WHERE YEAR(date) = $year ORDER BY `date` DESC;";
+      <div class="withCols">
+        <div class="col1">
+          <?php 
+            include("setup.php");
+            $year = date("Y");
+            $str = "SELECT * FROM `logs` WHERE YEAR(date) = $year ORDER BY `date` DESC;";
 
-          $entryresult = mysqli_query($con, $str);
-          if(!$entryresult){
-            $err = mysqli_error($con);
-            echo $err;
-            exit();
-          }
-          
-          $i = 0;
-          while ($entry = mysqli_fetch_assoc($entryresult)) {
-            $name = trim($entry["name"]);
-            $date = $entry["date"];
-            $datestr = date('F j', strtotime($date));
+            $entryresult = mysqli_query($con, $str);
+            if(!$entryresult){
+              $err = mysqli_error($con);
+              echo $err;
+              exit();
+            }
+            
+            $i = 0;
+            while ($entry = mysqli_fetch_assoc($entryresult)) {
+              $name = trim($entry["name"]);
+              $date = $entry["date"];
+              $datestr = date('F j', strtotime($date));
 
-            $imageFile = strtolower(str_replace(" ", "_", $name));
-            if (!file_exists("images/".$imageFile.".png")) {
-              $imageFile = "image".$i;
-              $i = ($i + 1) % 5;
-            }
-        ?>
-          <div class="game">
-            <a href="https://radinagames2020.tumblr.com/tagged/<?=$name?>"></a>
-            <img src="images/<?=$imageFile?>.png" />
-            <div class="info">
-              <time datetime="<?=$date?>"><?=$datestr?></time>
-              <div class="name"><?=$name?></div>
-            </div>
-          </div>
-        <?php } ?>
-      </div>
-      <div class="col2">
-        <?php 
-          // Upcoming games
-          $str = "SELECT * FROM `toplay` ORDER BY `status` DESC;";
-          $entryresult = mysqli_query($con, $str);
-          if(!$entryresult){
-            $err = mysqli_error($con);
-            echo $err;
-            exit();
-          }
-          $i = 0;
-          $lastStatus = -1;
-          while ($entry = mysqli_fetch_assoc($entryresult)) {
-            $name = trim($entry["name"]);
-            $status = $entry["status"];
-            if ($lastStatus != $status && $status == 1) {
-            ?>
-              <h3>Currently Playing</h3>
-            <?php
-            } elseif ($lastStatus != $status && $status == 0) {
-            ?>
-              <h3>To Play</h3>
-            <?php
-            }
-            $lastStatus = $status;
-            $imageFile = strtolower(str_replace(" ", "_", $name));
-            if (!file_exists("images/".$imageFile.".png")) {
-              $imageFile = "image".$i;
-              $i = ($i + 1) % 5;
-            }
+              $imageFile = strtolower(str_replace(" ", "_", $name));
+              if (!file_exists("images/".$imageFile.".png")) {
+                $imageFile = "image".$i;
+                $i = ($i + 1) % 5;
+              }
           ?>
-            <div class="game nolink">
+            <div class="game">
+              <a href="https://radinagames2020.tumblr.com/tagged/<?=$name?>"></a>
               <img src="images/<?=$imageFile?>.png" />
               <div class="info">
+                <time datetime="<?=$date?>"><?=$datestr?></time>
                 <div class="name"><?=$name?></div>
               </div>
             </div>
-        <?php
-          } ?>
+          <?php } ?>
+        </div>
+        <div class="col2">
+          <?php 
+            // Upcoming games
+            $str = "SELECT * FROM `toplay` ORDER BY `status` DESC;";
+            $entryresult = mysqli_query($con, $str);
+            if(!$entryresult){
+              $err = mysqli_error($con);
+              echo $err;
+              exit();
+            }
+            $i = 0;
+            $lastStatus = -1;
+            while ($entry = mysqli_fetch_assoc($entryresult)) {
+              $name = trim($entry["name"]);
+              $status = $entry["status"];
+              if ($lastStatus != $status && $status == 1) {
+              ?>
+                <h3>Currently Playing</h3>
+              <?php
+              } elseif ($lastStatus != $status && $status == 0) {
+              ?>
+                <h3>To Play (perhaps) (also see <a href="https://store.steampowered.com/wishlist/id/apdoyle/">here</a>)</h3>
+              <?php
+              }
+              $lastStatus = $status;
+              $imageFile = strtolower(str_replace(" ", "_", $name));
+              if (!file_exists("images/".$imageFile.".png")) {
+                $imageFile = "image".$i;
+                $i = ($i + 1) % 5;
+              }
+            ?>
+              <div class="game nolink">
+                <img src="images/<?=$imageFile?>.png" />
+                <div class="info">
+                  <div class="name"><?=$name?></div>
+                </div>
+              </div>
+          <?php
+            } ?>
+        </div>
       </div>
     </div>
   </body>
